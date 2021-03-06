3c9fa728bd2a071de4f07468145a6e11ce8d28e9
jcox1988 <jcox1988@gmail>
2021-03-06 15:43:37 -0600

    Changes to image name in save_images method

--- sensors/vision.py
+++ sensors/vision.py
@@ -51,13 +51,13 @@ class VisionSensorP3DX:
         return buffer
         # values are in the range of 0-1 (0=closest to sensor, 1=farthest from sensor).
 
-    def save_images(self, scene_name):
+    def save_images(self,scene_name):
         """
         Saves images from the vision sensor to the images directory in jpeg format.
         """
         if self.client_id != 1:
             print('Getting images')
-            dirname = "images\\" + scene_name + "_" + datetime.now().strftime("%d-%m-%YT%H%M%S")
+            dirname = "images\\"
             os.makedirs(dirname)
             while self.client_id != 1:
                 image = self.raw_image()
@@ -65,7 +65,7 @@ class VisionSensorP3DX:
                 img = np.array(image,dtype=np.uint8)
                 img.resize([self.image_resolution()[1], self.image_resolution()[0], 3])
                 img = cv2.flip(img, 0)
-                cv2.imwrite(dirname + "\{0}.jpg".format(time.time()), img)
+                cv2.imwrite(dirname + "\{0}.jpg".format(scene_name +"_"+ str(time.time())), img)
                 time.sleep(0.1)
                 if cv2.waitKey(0) == 0x1b:
                     break
